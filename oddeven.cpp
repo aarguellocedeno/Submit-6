1e9bdd71a411c3962b0767a517f04611f7a5c56f
JDRV17 <123598113+JDRV17@users.noreply.github.com>
2025-10-26 20:59:04 -0500

    Changed print and fixed race conditions
    Updated parallel sorting algorithm to use OpenMP reductions for marking swaps.

--- oddeven.cpp
+++ oddeven.cpp
@@ -1,46 +1,45 @@
 #include <iostream>
 #include <omp.h>
 
-int main(){
+int main() {
     int n;
     std::cin >> n;
     int A[n];
-    for (int i = 0; i < n; i++)
-    {
+    
+    for (int i = 0; i < n; i++) {
         std::cin >> A[i];
     }
+
     bool sorted = false;
-    
-    while (!sorted){
+
+    while (!sorted) {
         sorted = true;
         bool mark = false;
+
         //odd
-        #pragma parallel for shared(A)
-        for (int i = 1; i < n; i+=2){
-            if (A[i] > A[i + 1]){
-                int tmp = A[i];
-                A[i] = A[i + 1];
-                A[i + 1] = tmp;
+        #pragma omp parallel for shared(A) reduction(||:mark)
+        for (int i = 1; i < n - 1; i += 2) {
+            if (A[i] > A[i + 1]) {
+                std::swap(A[i], A[i + 1]);
                 mark = true;
             }
         }
+
         //even
-        #pragma parallel for shared(A)
-        for (int i = 0; i < n; i+=2){
-            if (A[i] > A[i + 1]){
-                int tmp = A[i];
-                A[i] = A[i + 1];
-                A[i + 1] = tmp;
+        #pragma omp parallel for shared(A) reduction(||:mark)
+        for (int i = 0; i < n - 1; i += 2) {
+            if (A[i] > A[i + 1]) {
+                std::swap(A[i], A[i + 1]);
                 mark = true;
             }
         }
-        if (mark){
-            sorted = false;
-        }
+
+        if (mark) sorted = false;
     }
 
-    for(int i = 0; i < n; i++)
-    {
-        std::cout << A[i];
+    for (int i = 0; i < n; i++) {
+        std::cout << A[i] << " ";
     }
+
+    return 0;
 }
